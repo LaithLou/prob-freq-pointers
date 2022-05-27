0ba74a4d62324fc47b820ff4055ea66cf00b9363
LaithLou <laithlouay@gmail.com>
2022-05-27 18:11:57 -0400

    solved can canConstructWord

--- can-construct-word.js
+++ can-construct-word.js
@@ -1,3 +1,27 @@
-// add whatever parameters you deem necessary & write doc comment
-function canConstructWord() {
+"use strict";
+
+/**take in a word in letters run them through a frequnecy counter
+ * and make sure that we can make words out of the letters. if not return
+ * false, otherwise return true.
+ */
+function canConstructWord(word, letters) {
+  const freq1 = ferquncyCounter(word);
+  const freq2 = ferquncyCounter(letters);
+
+  for (let letter in freq1) {
+    if (freq1[letter] !== freq2[letter]) {
+      return false;
+    }
+  }
+  console.log("freq1 = ", freq1, "freq2 = ", freq2);
+  return true;
+}
+
+function ferquncyCounter(string) {
+  let freqs = {};
+  for (let char of string) {
+    let val = freqs[char] || 0;
+    freqs[char] = val + 1;
+  }
+  return freqs;
 }
